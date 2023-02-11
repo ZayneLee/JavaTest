aeeefee28e7b32cb2d7b5373f0a437a91e862a31
Zayne <star1582@naver.com>
2023-02-11 11:45:34 +0900

    인터페이스와 추상메소드

---
+++ src/Printable.java
@@ -0,0 +1,16 @@
+// interface Printable {
+
+// public void print(String doc);
+
+// default void printCMYK(String doc) {
+// };
+// }
+
+// interface Printable {
+
+// int PAPER_WIDTH = 70;
+// int PAPER_HEIGHT = 120;
+
+// void print(String doc);
+
+// }
